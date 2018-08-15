e7f2a1241c47ea722a5e2bf9000eb39735cb3657
xunxingyuan <xunxingyuan@gmail.com>
2018-08-15 21:44:54 +0800

    add update

---
+++ src/knowledge/update.js
@@ -0,0 +1,9 @@
+const db = require('../controller/index')
+const Konw = db.Konwledge
+const Json = require('../tools/jsonResponse')
+
+module.exports = {
+    update: (ctx,next)=>{
+        
+    }
+}
